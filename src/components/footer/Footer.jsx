08801ddb09be7851aace3d07440c310442d8b311
Manmatha Mandal <manmatha.mandal@novagenixlab.com>
2025-02-19 09:47:43 +0530

    initial code push

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="ft-text">@ 2024 <i class="fa-sharp fa-regular fa-copyright"></i>All rights reserved | made with <i class="fa-sharp fa-solid fa-heart" id="heart"></i> by Team-A College</div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
