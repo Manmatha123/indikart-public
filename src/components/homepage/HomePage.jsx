6c6c7b28d878fb05a700b74c4fddf37451fb76a3
Soumya Ranjan Patra <soumyaranjanpatra009@gmail.com>
2025-02-22 19:49:11 +0530

    Adding HomePage

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./HomePage.css"
 import Footer from '../footer/Footer'
+import Mainhome from '../homepage/Mainhome'
 function HomePage() {
   return (
     <>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, ullam?</p>
+      <Mainhome/>
       <Footer/>
     </>
   )
